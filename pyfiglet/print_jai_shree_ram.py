8b57fe4b7f3a362915718cfbab10f19f63d8befd
subbarayudu-chakali <csubbu141@gmail.com>
2023-11-19 22:59:06 +0530

    added new directory pyfiglet

---
+++ pyfiglet/print_jai_shree_ram.py
@@ -0,0 +1,10 @@
+import pyfiglet
+
+
+def print_jai_shree_ram():
+    # Print the text art to the console
+    print(pyfiglet.figlet_format("Jai Shree Ram"))
+
+
+if __name__ == "__main__":
+    print_jai_shree_ram()
